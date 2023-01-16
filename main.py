e2c59f40ef2d2f3323367b66680a8d4fe7a45bf8
Vladimir901 <47474425+Vladimir901@users.noreply.github.com>
2023-01-16 14:33:56 +0300

    Поменял начальную функцию вывода

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_fio(surname, name, otchestvo):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'{surname} {name} {otchestvo}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_fio('Иванов', "Иван", "Иванович")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
